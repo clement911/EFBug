fc0a8e836a606a2bfaebb6a59f374b7f9eb15f8b
Clement <clement.gutel@gmail.com>
2022-03-01 07:49:14 +1100

    Add project files.

---
+++ EFBug/Migrations/20220228204538_InitialMigration.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFBug.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MyEntity",
+                columns: table => new
+                {
+                    StartedAt = table.Column<DateTime>(type: "datetime2(0)", nullable: false),
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MyEntity", x => new { x.StartedAt, x.Id });
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MyEntity");
+        }
+    }
+}
---
+++ EFBug/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+
+using (var ctx = new MyDbContext())
+{
+    ctx.Add(new MyEntity() { StartedAt = new DateTime(2000, 1, 1, 1, 1, 1, millisecond: 0) });
+    await ctx.SaveChangesAsync();//WORKS
+
+    ctx.Add(new MyEntity() { StartedAt = new DateTime(2000, 1, 1, 1, 1, 1, millisecond: 1) });
+    await ctx.SaveChangesAsync();//FAILS: The database operation was expected to affect 1 row(s), but actually affected 0 row(s); 
+}
+
+public class MyEntity
+{
+    [Column(TypeName = "datetime2(0)")]
+    public DateTime StartedAt { get; set; }
+
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; private set; }
+}
+
+public class MyDbContext : DbContext
+{
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<MyEntity>().HasKey(i => new { i.StartedAt, i.Id });
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+        optionsBuilder.UseSqlServer("Server=(local)\\sql2019;Database=EFBugRepro;Integrated Security=True;");
+    }
+}
+
